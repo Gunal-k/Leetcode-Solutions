7c9549b57f38856f7c9e9536c521b63dd5b8a006
Gunal K <gunalk.0007@gmail.com>
2026-04-01 23:44:53 +0530

    Add implementation for mySqrt function

---
+++ 69-sqrt(x).py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+
+        while l<=r:
+            mid = (l+r)//2
+            val = mid ** 2
+            
+            if val == x:
+                return mid
+            if val< x:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return r
