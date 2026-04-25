2bebb3846a36d7719d57573fb2ef8b204fb4f6b2
Gunal K <gunalk.0007@gmail.com>
2026-04-25 22:12:21 +0530

    Add implementation for MyStack class using queues in Java and Python

---
+++ 228-summary-ranges.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> ans = new ArrayList<>();
+        int i = 0;
+
+        while(i < nums.length){
+            int start = nums[i];
+
+            while(i < (nums.length - 1) && nums[i+1] == nums[i] + 1){
+                i++;
+            }
+            if(start != nums[i]) ans.add(""+start+"->"+nums[i]);
+            else ans.add("" + nums[i]);
+            
+            i++;
+        }
+        return ans;
+    }
+}
