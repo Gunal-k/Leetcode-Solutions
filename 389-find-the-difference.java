d72fa7c3589009c839fcbc99614d216ef4df8a1f
Gunal K <gunalk.0007@gmail.com>
2026-05-16 20:56:41 +0530

    Implement findTheDifference function to find the added character between two strings in Java and Python

---
+++ 389-find-the-difference.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int res = 0;
+        for(char c : s.toCharArray()){
+            res = res ^ (int) c;
+        }
+        for(char c : t.toCharArray()){
+            res = res ^ (int) c;
+        }
+        return (char)res;
+    }
+}
