ec17553d9f43311570cc0bdba23f3ef86e0f8b91
Gunal K <146356261+Gunal-k@users.noreply.github.com>
2026-07-26 20:44:26 +0530

    Implement solution to count prime set bits in range

---
+++ 762-prime-umber-of-set-bits-in-binary-representation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        def is_prime(n):
+            if n == 1:
+                return False
+            if n == 2:
+                return True
+            if n%2 == 0:
+                return False
+            for i in range(3,int(n**0.5 + 1),2):
+                if n % i == 0:
+                    return False
+            return True
+        ans = 0
+        for i in range(left,right+1):
+            val = bin(i).count("1")
+            if is_prime(val):
+                ans+=1
+        return ans
