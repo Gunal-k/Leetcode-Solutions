74b3fd84ad43650f50dc5ce45eb1cd4310229e2b
Gunal K <146356261+Gunal-k@users.noreply.github.com>
2026-08-03 22:04:14 +0530

    Implement mostCommonWord function to find frequent words

---
+++ 819-most-common-word.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+
+        banned = set(banned)
+
+        for ch in "!?',;.":
+            paragraph = paragraph.replace(ch, " ")
+
+        words = paragraph.lower().split()
+
+        count = Counter()
+
+        for word in words:
+            if word not in banned:
+                count[word] += 1
+
+        return max(count, key=count.get)
