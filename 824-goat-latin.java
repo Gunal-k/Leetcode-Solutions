59579e6bacf97d8f2a0fe4230785deafe7973f4a
Gunal K <146356261+Gunal-k@users.noreply.github.com>
2026-08-05 21:53:56 +0530

    Add Goat Latin conversion method
    Implement Goat Latin conversion for a given sentence.

---
+++ 824-goat-latin.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+
+        String[] words = sentence.split(" ");
+        String vowels = "aeiouAEIOU";
+
+        for (int i = 0; i < words.length; i++) {
+
+            String word = words[i];
+
+            if (vowels.indexOf(word.charAt(0)) != -1) {
+                word += "ma";
+            } else {
+                word = word.substring(1) + word.charAt(0) + "ma";
+            }
+
+            word += "a".repeat(i + 1);
+
+            words[i] = word;
+        }
+
+        return String.join(" ", words);
+    }
+}
