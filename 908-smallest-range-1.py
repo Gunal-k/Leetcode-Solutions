1f152d38f8fcef26aae6a517caad94fc59919461
Gunal K <146356261+Gunal-k@users.noreply.github.com>
2026-08-22 23:34:17 +0530

    Simplify smallestRangeI method implementation

--- 908-smallest-range-1.py
+++ 908-smallest-range-1.py
@@ -1,9 +1,3 @@
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
-        mini = maxi = nums[0]
-
-        for num in nums:
-            mini = min(mini,num)
-            maxi = max(maxi,num)
-        
-        return max(0,maxi-mini-2*k)
+        return max(0,max(nums)-min(nums)-2*k)
