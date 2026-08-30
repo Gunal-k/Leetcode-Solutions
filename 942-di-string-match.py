5c6d69a8a773d9d570d39cb16863d7653283e925
Gunal K <146356261+Gunal-k@users.noreply.github.com>
2026-08-30 23:23:20 +0530

    Add solution for DI String Match
    Implement a solution for the DI String Match problem.

---
+++ 942-di-string-match.py
@@ -0,0 +1,17 @@
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        low = 0
+        high = len(s)
+        ans = []
+
+        for c in s:
+            if c == 'I':
+                ans.append(low)
+                low += 1
+            else:
+                ans.append(high)
+                high -= 1
+
+        ans.append(low)
+
+        return ans
