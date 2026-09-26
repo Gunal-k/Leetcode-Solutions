aa2fe407f8c69a60388165c7f1339e1fe84dc02e
Gunal K <146356261+Gunal-k@users.noreply.github.com>
2026-09-26 23:33:00 +0530

    Implement findOcurrences method in Solution class

---
+++ 1078-Occurrencse-after-bigram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> list[str]:
+        ans = []
+        words = text.split()
+        i = 0
+        while i < len(words)-2:
+            if words[i] == first and words[i+1] == second:
+                ans.append(words[i+2])
+            i+=1
+        return ans
